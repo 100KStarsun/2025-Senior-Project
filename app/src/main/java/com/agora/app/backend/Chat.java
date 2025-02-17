0d95d574983fe4008050ee54516ac9c8e2bdbc34
100KStarsun <57645803+100KStarsun@users.noreply.github.com>
2025-02-17 15:29:55 -0500

    added files back bc prev commit didnt
    grrrr

---
+++ app/src/main/java/com/agora/app/backend/Chat.java
@@ -0,0 +1,66 @@
+package com.agora.app.backend;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.UUID;
+
+public class Chat implements Serializable {
+
+    public static String chatsTableName = "agora_chats";
+
+    private UUID chatUUID;
+    private String username1;
+    private String username2;
+    private int order;
+    private ArrayList<Message> messages;
+
+    // this constructor should be used when creating a new chat object with no data from the db
+    public Chat (String username1, String username2, int order) {
+        this.chatUUID = UUID.randomUUID();
+        this.username1 = username1;
+        this.username2 = username2;
+        this.order = Chat.getOrder(username1, username2);
+        this.messages = new ArrayList<Message>();
+    }
+
+    // this constructor should only be used when we have data from the db already
+    public Chat (UUID uuid, String username1, String username2, int order, ArrayList<Message> messages) {
+        this.chatUUID = uuid;
+        this.username1 = username1;
+        this.username2 = username2;
+        this.order = order;
+        this.messages = messages;
+    }
+
+    private Chat addMessage (Message msg) {
+        if (this.isMessageGoingToFitInCurrentChat(msg)) {
+            this.messages.add(msg);
+            return this;
+        }
+        Chat newChat = new Chat(this.username1, this.username2, Chat.getOrder(this.username1, this.username2));
+        newChat.messages.add(msg); //safe to force this because we know this message will fit - though this means we need to enforce reasonable message limits
+        User.getUserFromUsername(this.username1).addChat(this.username2, newChat.order);
+        User.getUserFromUsername(this.username2).addChat(this.username1, newChat.order);
+        return newChat;
+    }
+
+    private boolean isMessageGoingToFitInCurrentChat (Message msg) {
+        // TODO: Add logic so that we check if the chat item in the db is approaching max size (390KB cutoff, 400KB max)
+        return true;
+    }
+
+    private String packUsernames() {
+        return "(" + this.username1 + "," + this.username2 + ")";
+    }
+
+    // gets the 2 usernames who are participating in the chat
+    // gets the user object associated with username1 and checks to see if they have a chat with username2 already
+    // if so, then we figure out how many chat objects this user already has and return that.
+    // if none, this is first chat with username2 and we return zero
+    public static int getOrder (String username1, String username2) {
+        User user1 = User.getUserFromUsername(username1);
+        int order = user1.getChats().containsKey(username2) ? user1.getChats().get(username2).size() : 0;
+        return order;
+    }
+
+}
---
+++ app/src/main/java/com/agora/app/backend/dynamoDB/DynamoDBHandler.java
@@ -0,0 +1,33 @@
+package com.agora.app.backend.dynamoDB;
+
+import java.util.Map;
+
+import javax.swing.plaf.synth.Region;
+
+import software.amazon.awssdk.services.dynamodb.model.DynamoDbException;
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+
+
+public class DynamoDBHandler {
+
+    public static Region awsRegion = Region.US_EAST_2; // We will only be using stuff in the us_east_2 region as this region is based in Ohio
+
+    /*
+     * 
+     */
+    public static Map<String, AttributeValue> getItem (String tableName, String key, String keyValue) {
+        try (DynamoDbEnhancedClient ddbec = DynamoDbEnhancedClient.create();) {
+            // need to build `request` here
+            Map<String, AttributeValue> returnedItem = ddbec.getItem(request).item();
+            // if there is no matching item, `returnedItem` will be `null`
+            return returnedItem;
+
+        } catch (DynamoDbException e) {
+            System.err.println(e.getMessage());
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
