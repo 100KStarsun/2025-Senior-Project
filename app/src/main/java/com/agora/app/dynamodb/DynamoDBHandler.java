1a4d7f1a83b6d95f0610cde4d9a1e15162085aff
100KStarsun <57645803+100KStarsun@users.noreply.github.com>
2025-02-25 19:57:50 -0500

    start of comments
    (pushing so i can work on another computer)

--- app/src/main/java/com/agora/app/dynamodb/DynamoDBHandler.java
+++ app/src/main/java/com/agora/app/dynamodb/DynamoDBHandler.java
@@ -16,22 +16,55 @@ import software.amazon.awssdk.enhanced.dynamodb.model.GetItemEnhancedRequest;
 
 public class DynamoDBHandler {
 
+    /**
+     * The DynamoDB table name for storing users
+     */
     public static String usersTableName = "agora_users";
+    /**
+     * The DynamoDB table name for storing passwords
+     */
     public static String passwordsTableName = "agora_passwords";
+    /**
+     * The DynamoDB table name for storing chat objects
+     */
     public static String chatsTableName = "agora_chats";
+    /**
+     * The DynamoDB table name for storing products
+     */
     public static String productsTableName = "agora_products";
+    /**
+     * The AWS Region where our DynamoDB instance is, also where our credentials are valid
+     */
     public static Region awsRegion = Region.US_EAST_2; // We will only be using stuff in the us_east_2 region as this region is based in Ohio
 
+    /**
+     * Creates the request object that asks for the item with the specified partition key
+     *
+     * @param partitionKey A unique string that can be used to find the item in the DynamoDB table
+     * @return a {@code GetItemEnhancedRequest} object that is the properly formatted object for retrieving an item from a DynamoDB table
+     */
     public static GetItemEnhancedRequest makeRequestFromPartitionKey (String partitionKey) {
         return GetItemEnhancedRequest.builder().key(Key.builder().partitionValue(partitionKey).build()).build();
     }
 
+    /**
+     * Creates the {@code DynamoDbEnhancedClient} required to interface with the DynamoDB tables
+     *
+     * @return a {@code DynamoDbEnhancedClient} object that is set up to be based in the AWS Region specified earlier
+     */
     public static DynamoDbEnhancedClient makeDynamoClient () {
         DynamoDbClient basicClient = DynamoDbClient.builder().region(awsRegion).build();
         return DynamoDbEnhancedClient.builder().dynamoDbClient(basicClient).build();
     }
 
-    public static User getUserItem (String username) throws NullPointerException {
+    /**
+     * Retrieves a user object from the database, given a specific username
+     *
+     * @param username The username of the user (their CWRU Network ID)
+     * @return The {@code User} object (if it exists) that has the specified username, {@code null} if there is no User object in the database with that username
+     * @throws NullPointerException
+     */
+    public static User getUserItem (String username) {
         try {
             DynamoDbEnhancedClient enhancedClient = makeDynamoClient();
             DynamoDbTable<UserWrapper> userTable = enhancedClient.table(DynamoDBHandler.usersTableName, TableSchema.fromBean(UserWrapper.class));
