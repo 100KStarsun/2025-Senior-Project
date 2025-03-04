5331283f084767bbd73c375a17802bb646bf2f4a
snm67 <125221354+snm67@users.noreply.github.com>
2025-03-04 00:16:44 -0500

    added core components to hamburger pages

--- app/src/main/java/com/agora/app/frontend/SettingsActivity.java
+++ app/src/main/java/com/agora/app/frontend/SettingsActivity.java
@@ -3,10 +3,12 @@ package com.agora.app.frontend;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.agora.app.R;
+import com.agora.app.databinding.ActivitySettingsBinding;
 
 import java.util.Objects;
 
@@ -19,5 +21,18 @@ public class SettingsActivity extends AppCompatActivity {
         Objects.requireNonNull(getSupportActionBar()).hide();
         Button buttonUserInfo = findViewById(R.id.buttonUserInfo);
         buttonUserInfo.setOnClickListener(v -> startActivity(new Intent(SettingsActivity.this, UserInfoActivity.class)));
+
+        CheckBox simpleCheckBox = (CheckBox) findViewById(R.id.simpleCheckBox);
+
+        ActivitySettingsBinding binding = ActivitySettingsBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.materialSwitch.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if (isChecked) {
+                // The switch is checked.
+            } else {
+                // The switch isn't checked.
+            }
+        });
     }
 }
