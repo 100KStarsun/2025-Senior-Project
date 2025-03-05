8edea6e47e5e59f2543c7cbe661f4b98f8e5aa3b
100KStarsun <57645803+100KStarsun@users.noreply.github.com>
2025-03-05 12:28:00 -0500

    implemented product wrapper
    also remade the dynamo table so that the partition key is now "uuid" and
    there is no more sort key (so that it conforms to the style of the other
    tables)
    
    note that the partition key is still of type "String" and "uuid"
    represents the productUUID not the sellerUUID

--- app/src/main/java/com/agora/app/backend/base/Product.java
+++ app/src/main/java/com/agora/app/backend/base/Product.java
@@ -1,8 +1,14 @@
 package com.agora.app.backend.base;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.time.Instant;
 import java.util.ArrayList;
+import java.util.Base64;
 import java.util.UUID;
 
 public class Product implements Serializable {
@@ -24,4 +30,42 @@ public class Product implements Serializable {
     private boolean isAcceptingCash;
     private boolean isTradable;
     private ArrayList<UUID> previousBuyers;
+    private String[] tags;
+
+    /**
+     * Used by the {@code ProductWrapper} class to turn the base64-encoded string back into a {@code Product}, using an {@code ObjectInputStream}, {@code ByteArrayInputStream}, and the {@code Base64.Decoder} class.
+     *
+     * @param encodedProduct a base64 string that should have been created by the {@code toBase64String()} method
+     * @return a {@code Product} object that holds all the data it did before it was converted to a base64 string
+     */
+    public static Product createFromBase64String (String encodedProduct) {
+        Base64.Decoder decoder = Base64.getDecoder();
+        byte[] decodedBytes = decoder.decode(encodedProduct);
+        try (ByteArrayInputStream bytesIn = new ByteArrayInputStream(decodedBytes); ObjectInputStream objectIn = new ObjectInputStream(bytesIn)) {
+            return (Product) objectIn.readObject();
+        } catch (IOException | ClassNotFoundException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    /**
+     * Used by the {@code ProductWrapper} class to turn a {@code Product} object into a base64-encoded string for easy database storage. This is done using a {@code ObjectOutputStream}, {@code ByteArrayOutputStream}, and the {@code Base64.Encoder} class.
+     *
+     * @return a string containing the base64 representation of the {@code Product} object this method was called using.
+     */
+    public String toBase64String () {
+        Base64.Encoder encoder = Base64.getEncoder();
+        try (ByteArrayOutputStream bytesOut = new ByteArrayOutputStream(); ObjectOutputStream objectOut = new ObjectOutputStream(bytesOut)) {
+            objectOut.writeObject(this);
+            return encoder.encodeToString(bytesOut.toByteArray());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        return null;
+    }
+
+    public UUID getUUID () {
+        return this.productUUID;
+    }
 }
--- app/src/main/java/com/agora/app/dynamodb/ProductWrapper.java
+++ app/src/main/java/com/agora/app/dynamodb/ProductWrapper.java
@@ -1,4 +1,77 @@
 package com.agora.app.dynamodb;
 
+import com.agora.app.backend.base.Product;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbAttribute;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+
+@DynamoDbBean
 public class ProductWrapper {
+    /**
+     * The uuid of the {@code Product} object. This is unique among all products and as such, is the partition key for the product table in the database.
+     */
+    private String uuid;
+    /**
+     * The Base64-String-based representation of the entire {@code Product} object, as created by {@code toBase64String()} in the {@code Product} class.
+     *
+     * @see Product#toBase64String()
+     */
+    private String productBase64;
+
+    /**
+     * Default constructor, not used, only included to fully implement the {@code @DynamoDbBean} requirements. Creates empty strings for {@code uuid} and {@code productBase64}.
+     */
+    public ProductWrapper () {
+        this.uuid = "";
+        this.productBase64 = "";
+    }
+
+    /**
+     * Grabs the uuid and base64 String representation of the {@code Product} object passed and makes a DynamoDB-friendly object from it
+     *
+     * @param product The {@code Product} object to be wrapped before being put into the DynamoDB table for products
+     */
+    public ProductWrapper (Product product) {
+        this.uuid = product.getUUID().toString();
+        this.productBase64 = product.toBase64String();
+    }
+
+    /**
+     * Returns the uuid of the {@code Product} object being (un)wrapped
+     *
+     * @return the {@code String} object that holds the uuid of the {@code Product} object being (un)wrapped
+     */
+    @DynamoDbPartitionKey
+    @DynamoDbAttribute("uuid")
+    public String getUuid () {
+        return this.uuid;
+    }
+
+    /**
+     * Returns the base64-encoded {@code Product} object as a {@code String} object
+     *
+     * @return The base64-encoded {@code String} object that represents the {@code Product} object being (un)wrapped
+     */
+    @DynamoDbAttribute("base64")
+    public String getProductBase64 () {
+        return this.productBase64;
+    }
+
+    /**
+     * Manually sets the uuid of the wrapped {@code Product} object. Only implemented to satisfy {@code @DynamoDbBean} requirements.
+     *
+     * @param uuid the username of the {@code Product} object
+     */
+    public void setUuid (String uuid) {
+        this.uuid = uuid;
+    }
+
+    /**
+     * Manually sets the base64 string of the wrapped {@code Product} object. Only implemented to satisfy {@code @DynamoDbBean} requirements.
+     *
+     * @param productBase64 the base64 string representation of the {@code Product} object
+     */
+    public void setProductBase64 (String productBase64) {
+        this.productBase64 = productBase64;
+    }
 }
